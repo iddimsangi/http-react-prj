cb1e343fdedfa63cd427814c312ec71e97ab90db
iddimsangi <adammsangi@gmail.com>
2021-03-15 13:31:30 +0300

    display a particular data based on ID

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -2,22 +2,28 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './FullPost.css'
 class FullPost extends Component {
-    // state = {
-    //     loadedData:null
-    // }
+    state = {
+        loadedData:null
+    }
 componentDidUpdate(){
     axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
     .then(resp =>{
-        console.log(resp)
+        // console.log(resp);
+        this.setState({
+            loadedData:resp.data
+        })
     })
 }
     render () {
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
         if(this.props.id){
+           post = <p style={{textAlign:'center'}}>loading...</p>;
+        }
+        if(this.state.loadedData){
             post = (
                 <div className="FullPost">
-                    <h1>{this.props.title}</h1>
-                    <p>{this.props.titleBody}</p>
+                    <h1>{this.state.loadedData.title}</h1>
+                    <p>{this.state.loadedData.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
