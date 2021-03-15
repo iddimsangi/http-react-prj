5787fc1e64788fbc9fa46fe57bae9f39121445e8
iddimsangi <adammsangi@gmail.com>
2021-03-15 19:02:06 +0300

    render header layout from antd

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,9 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import { Layout, Menu } from 'antd';
+
+const { Header } = Layout;
 
 class Blog extends Component {
     state ={
@@ -55,6 +58,14 @@ class Blog extends Component {
      
         return (
             <div>
+                <Header>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+               </Header>
                 <section className="Posts">
                     {/* <Post />
                     <Post />
